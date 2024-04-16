343a3e4077886069d93f1d6294b2621328527d68
Paulo Marques <paulomarquesexecultivo@gmail.com>
2024-04-16 07:00:10 -0300

    Atualizacao no setup das tabelas do banco postgres. Criacao da tabela users e ajustes na tabela short_links

--- src/setup.ts
+++ src/setup.ts
@@ -2,11 +2,24 @@ import { sql } from "./lib/postgres";
 
 async function setup() {
   await sql/*sql*/ `
+    CREATE TABLE IF NOT EXISTS users (
+      id TEXT PRIMARY KEY UNIQUE,
+      name TEXT,
+      email TEXT UNIQUE,
+      password TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+  `;
+
+  await sql/*sql*/ `
     CREATE TABLE IF NOT EXISTS short_links (
-      id SERIAL PRIMARY KEY,
+      id TEXT PRIMARY KEY UNIQUE,
       code TEXT UNIQUE,
       original_url TEXT,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      user_id TEXT REFERENCES users(id) ON DELETE CASCADE,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
   `;
 
